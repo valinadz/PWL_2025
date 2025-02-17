1ec0f73a0173353d460008e5e5b17be959573768
Nervalina Adzra Nora Aqilla <nervalinaadzranoraaqilla@gmail.com>
2025-02-17 13:56:39 +0700

    js 2

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,36 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'NIM: 2341760066 - Nama: Nervalina Adzra Nora Aqilla';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function ($name = 'Valina') {
+    return 'Nama saya ' . $name;
+});
+
